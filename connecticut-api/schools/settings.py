61e7508959b14296aba796d0cae8db4e5b60bd9f
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-07-23 18:07:03 -0400

    Add files via upload

--- connecticut-api/schools/settings.py
+++ connecticut-api/schools/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-s0+=*l_bg9p#%6kyh-&$z1bk7@jk=9tnp@5*+vtd%ag!(0!4_l
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -113,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
